def5906fdf699e3966aa2bc489afd66d76947055
gro5-AberUni <74013177+gro5-AberUni@users.noreply.github.com>
2024-11-25 16:55:59 +0000

    Update fuzzy.py
    add fuzzy classification module and functions

--- tropwet/fuzzy.py
+++ tropwet/fuzzy.py
@@ -1,1 +1,25 @@
+import ee
+import geemap.core as geemap
 
+ee.Authenticate()
+ee.Initialize(project='ee-gregoryoakessentone')
+
+def fuzzy_gt(img,lowBound,upBound):
+  fuzzyOut = ee.Image(unmixImg.subtract(lowBound)).multiply(ee.Number(1).divide(upBound-lowBound))
+  fuzzyOut = fuzzyOut.where(unmixImg.lt(lowBound),0)
+  fuzzyOut = fuzzyOut.where(unmixImg.gt(upBound),1)
+  return fuzzyOut
+
+def fuzzy_lt(img,lowBound,upBound):
+  fuzzyOut = ee.Image(unmixImg.subtract(upBound)).multiply(ee.Number(1).divide(lowBound-upBound))
+  fuzzyOut = fuzzyOut.where(unmixImg.lt(lowBound),1)
+  fuzzyOut = fuzzyOut.where(unmixImg.gt(upBound),0)
+  return fuzzyOut
+
+def fuzzy_and(imgCol):
+  minImg = imgCol.min()
+  return minImg
+
+def fuzzy_or(imgCol):
+  maxImg = imgCol.max()
+  return maxImg
