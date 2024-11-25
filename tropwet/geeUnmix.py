51e7b161a13ca3034343cbc1c1785636ad7a8213
gro5-AberUni <74013177+gro5-AberUni@users.noreply.github.com>
2024-11-25 14:36:21 +0000

    Update geeUnmix.py
    added Unmix Script

--- tropwet/geeUnmix.py
+++ tropwet/geeUnmix.py
@@ -1,1 +1,9 @@
+import ee
 
+def unmix(img,endmembers,sumToOne:bool=True, nonNegative:bool=True,scaleFactor:int=10000):
+  unmixedImage = median_composite.unmix(endmembers,True,True);
+  bandNames2 = ['water', 'veg', 'bare','burn'];
+  unmixedImage = unmixedImage.rename(bandNames2);
+  unmixedImage = unmixedImage.select(bandNames2);
+  unmixedImage = unmixedImage.multiply(scaleFactor)
+  return unmixedImage
